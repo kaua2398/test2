a476487628342655fa9eca9ea37e0e2089263aed
kaua2398 <145510083+kaua2398@users.noreply.github.com>
2025-10-27 09:05:57 -0300

    Update SecurityFilter.java

--- src/main/java/com/valeshop/timesheet/infra/security/SecurityFilter.java
+++ src/main/java/com/valeshop/timesheet/infra/security/SecurityFilter.java
@@ -39,6 +39,10 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+                if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         try {
             String token = recoverToken(request);
